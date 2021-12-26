7bc61a2bf749274ddbff0d748f58490311741f3a
Aditya Agarwala <aditya17022002@gmail.com>
2021-12-26 21:27:48 +0530

    Created Note View Model

--- app/src/main/java/com/example/mynotesapp/NoteDatabase.kt
+++ app/src/main/java/com/example/mynotesapp/NoteDatabase.kt
@@ -8,7 +8,7 @@ import androidx.room.RoomDatabase
 @Database(entities = arrayOf(Note::class), version = 1, exportSchema = false)
 abstract class NoteDatabase:RoomDatabase() {
 
-    abstract fun noteDao():NoteDao
+    abstract fun getNoteDao():NoteDao
 
     companion object {
         // Singleton prevents multiple instances of database opening at the
